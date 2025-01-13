c40dfb276307dea0f8b70745b95d2dc0d3649d76
mamuka0 <mamoodikadze1@gmail.com>
2025-01-13 22:04:50 +0400

    commit

--- day 28/cw/hw.py
+++ day 28/cw/hw.py
@@ -1,1 +1,8 @@
-# nothing
+def manual_index(main_str, search_str):
+    
+    for i in range(len(main_str) - len(search_str) + 1):
+        
+        if main_str[i:i+len(search_str)] == search_str:
+            return i
+   
+    return -1
